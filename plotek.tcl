cd66d7d8172b951809c5b2483ad2a2dea8088053
Neil Johnson <neil.johnson71@gmail.com>
2020-10-23 17:08:36 +0100

    Multiplot now working
    Multiple plots now handled.
    Needs further work on trace label positioning, and would like a monochrome option.

--- plotek.tcl
+++ plotek.tcl
@@ -24,9 +24,11 @@ package require struct::matrix
 proc readDataFile {fname} {
 	global data
 	
+	puts "Reading $fname ..."
 	set chan [open $fname]
 	csv::read2matrix $chan data  , auto
 	close $chan
+	puts "Done."
 }
 
 # ############################################################################
@@ -36,20 +38,26 @@ proc readDataFile {fname} {
 proc scanDataFile {} {
 	global data
 	global plotList
+	global plotEn
 	
 	set plotList {}
 	set trc 0
 	set i 0
 	
+	puts "Scanning data..."
 	for {set c 0} {$c < [data columns]} {incr c 6} {
 		set name [data get cell [expr $c + 1] 6]
+		puts "  found dataset $name"
 		if {[string match "Glitch*" $name] == 0} {
-			.menubar.plot add checkbutton -label "$i: $name" -variable plot$trc
-			lappend plotList plot$trc 
+			.menubar.plot add checkbutton -label "$i: $name" -variable plotEn($trc)
+			set plotEn($trc) 0
+			lappend plotList $trc 
+			puts "  $trc: adding plot"
 			incr i
 		}
 		incr trc
 	}
+	puts "Done."
 }
 
 # ############################################################################
@@ -59,92 +67,110 @@ proc scanDataFile {} {
 proc generateScopeTraces {title} {
 	global data
 	global plotList
-	
-	foreach l $plotList {
-		puts $l
-	}
-	
-	return
+	global plotEn
 
+	puts "Generating Scope Traces..."
+
+	set i 0
 	set ofile [open "tekplots.cmd" w]
-
 	set rows [data rows]
-	set base [expr $tracenum * 6]
+
+	set plotcommand ""
+
+	foreach l $plotList {
+		set enabled $plotEn($l)
+		puts "Plot $i @ $l == $enabled"
 	
-	# Extract trace parameters
-	set params [data get rect [expr $base + 1] 0 [expr $base + 1] 15]
-	set recLength 		[lindex $params 0]
-	set sampleInterval 	[lindex $params 1]
-	set triggerPoint	[lindex $params 2]
-	set source			[lindex $params 6]
-	set vertUnits		[lindex $params 7]
-	set vertScale		[lindex $params 8]
-	set vertOffset		[lindex $params 9]
-	set horizUnits		[lindex $params 10]
-	set horizScale		[lindex $params 11]
-	set ptFmt			[lindex $params 12]
-	set yZero			[lindex $params 13]
-	set probeAtten		[lindex $params 14]
-	set note			[lindex $params 15]
+		set tracenum $l
+		set base [expr $tracenum * 6]
 
-	puts $ofile "\$ScopeTrace << EOD"
-	for {set row 0} {$row < $rows} {incr row} {
-		set point [data get rect [expr $base + 3] $row [expr $base + 4] $row]
-		puts $ofile [format "%.6f %.6f %.6f" [lindex [lindex $point 0] 0] [expr [lindex [lindex $point 0] 1] + $vertScale * $yZero] $vertOffset]
-	}
-	puts $ofile "EOD"
+		# Extract trace parameters
+		set params [data get rect [expr $base + 1] 0 [expr $base + 1] 15]
+		set recLength 		[lindex $params 0]
+		set sampleInterval 	[lindex $params 1]
+		set triggerPoint	[lindex $params 2]
+		set source			[lindex $params 6]
+		set vertUnits		[lindex $params 7]
+		set vertScale		[lindex $params 8]
+		set vertOffset		[lindex $params 9]
+		set horizUnits		[lindex $params 10]
+		set horizScale		[lindex $params 11]
+		set ptFmt			[lindex $params 12]
+		set yZero			[lindex $params 13]
+		set probeAtten		[lindex $params 14]
+		set note			[lindex $params 15]
 
-	if {$title != ""} {
-		#puts "set title \"[data get cell [expr $base + 1] 6] ([data get cell [expr $base + 1] 16])\""
-		puts $ofile "set title \"$title\""
-		puts $ofile "show title"
+		if {$i == 0} {
+			puts "Setting up..."
+
+			if {$title != ""} {
+				#puts "set title \"[data get cell [expr $base + 1] 6] ([data get cell [expr $base + 1] 16])\""
+				puts $ofile "set title \"$title\""
+				puts $ofile "show title"
+			}
+
+			set div $horizScale
+			set mult ""
+			if {$div < 1.0} {
+				set div [expr $div * 1000]
+				set mult "m"
+			}
+			if {$div < 1.0} {
+				set div [expr $div * 1000]
+				set mult "u"
+			}
+			if {$div < 1.0} {
+				set div [expr $div * 1000]
+				set mult "n"
+			}
+			set div [expr int($div)]
+			puts $ofile "set xlabel \"$div $mult$horizUnits/div\""
+			puts $ofile "set xtics format \"\""
+
+			# Note to self: need to accomodate probe attenuation
+
+			set div $vertScale
+			set mult ""
+			if {$div < 1.0} {
+				set div [expr $div * 1000]
+				set mult "m"
+			}
+			if {$div < 1.0} {
+				set div [expr $div * 1000]
+				set mult "u"
+			}
+			if {$div < 1.0} {
+				set div [expr $div * 1000]
+				set mult "n"
+			}
+			set div [expr int($div)]
+			puts $ofile "set ylabel \"$div $mult$vertUnits/div\""
+			puts $ofile "set yrange \[ -[expr $vertScale * 4] : [expr $vertScale * 4]\]"
+			puts $ofile "set ytics $vertScale format \"\""
+
+			puts $ofile "set grid xtics ytics"
+
+			puts $ofile "set terminal gif"
+			puts $ofile "set output \"tekplots.gif\""
+		}
+
+		if {$enabled} {
+			puts "Processing trace $tracenum"
+			puts $ofile "\$ScopeTrace$i << EOD"
+			for {set row 0} {$row < $rows} {incr row} {
+				set point [data get rect [expr $base + 3] $row [expr $base + 4] $row]
+				puts $ofile [format "%.6f %.6f %.6f" [lindex [lindex $point 0] 0] [expr [lindex [lindex $point 0] 1] + $vertScale * $yZero] $vertOffset]
+			}
+			puts $ofile "EOD"
+			set thisplot " \$ScopeTrace$i using 1:2 with lines title \"$source\", \$ScopeTrace$i using 1:3 with lines title \"\", "
+			append plotcommand $thisplot
+		}
+		incr i
 	}
 
-	set div $horizScale
-	set mult ""
-	if {$div < 1.0} {
-		set div [expr $div * 1000]
-		set mult "m"
-	}
-	if {$div < 1.0} {
-		set div [expr $div * 1000]
-		set mult "u"
-	}
-	if {$div < 1.0} {
-		set div [expr $div * 1000]
-		set mult "n"
-	}
-	set div [expr int($div)]
-	puts $ofile "set xlabel \"$div $mult$horizUnits/div\""
-	puts $ofile "set xtics format \"\""
+	puts $ofile "plot $plotcommand"
 
-	# Note to self: need to accomodate probe attenuation
-
-	set div $vertScale
-	set mult ""
-	if {$div < 1.0} {
-		set div [expr $div * 1000]
-		set mult "m"
-	}
-	if {$div < 1.0} {
-		set div [expr $div * 1000]
-		set mult "u"
-	}
-	if {$div < 1.0} {
-		set div [expr $div * 1000]
-		set mult "n"
-	}
-	set div [expr int($div)]
-	puts $ofile "set ylabel \"$div $mult$vertUnits/div\""
-	puts $ofile "set yrange \[ -[expr $vertScale * 4] : [expr $vertScale * 4]\]"
-	puts $ofile "set ytics $vertScale format \"\""
-
-	puts $ofile "set grid xtics ytics"
-	
-   puts $ofile "set terminal gif"
-   puts $ofile "set output \"tekplots.gif\""
-   
-   puts $ofile "plot \$ScopeTrace using 1:2 with lines title \"$source\", \$ScopeTrace using 1:3 with lines title \"\""
+	puts "Done."
    
    close $ofile
 }
@@ -199,13 +225,16 @@ proc m_openFile {} {
 # Menu action: plot a trace
 #
 proc m_plotTraces {} {
+	puts "Sending to gnuplot..."
    generateScopeTraces ""
-   if { ![catch "exec gnuplot tekplots.cmd -" msg] } {
+   if { ![catch "exec gnuplot tekplots.cmd" msg] } {
+	   puts "Loading plot graphic..."
       image create photo plot -file "tekplots.gif"
       .c config -width [image width plot] -height [image height plot]
       .c create image [expr [image width plot]/2] [expr [image height plot]/2] \
          -image plot -anchor c
    }
+   puts "Done."
 }
 
 # ############################################################################
